592d2a44137bec469cc06cd9e6bd9814b961684a
Leadgq <2643336540@qq.com>
2026-01-12 15:06:40 +0800

    feat(结果组件): 添加键盘上下箭头导航功能
    使用 useImmer 管理当前选中索引，通过监听键盘事件实现上下导航

--- code-tips/src/renderer/src/components/result/index.tsx
+++ code-tips/src/renderer/src/components/result/index.tsx
@@ -1,20 +1,31 @@
 import { useCodeContext } from "@renderer/hook"
 import { useEffect } from "react"
+import { useImmer } from "use-immer"
 
 export function Result(): React.JSX.Element {
     const { resultData } = useCodeContext()
-    useEffect(() => {
-       const handleKeyDown = (e: KeyboardEvent) => {
-            console.log('按下');
+    const [currentIndex, setCurrentIndex] = useImmer(0);
+    const handleKeyDown = (e: KeyboardEvent) => {
+        switch(e.code){
+            case 'ArrowUp':
+                setCurrentIndex(prev=> prev-1);
+                break;
+            case 'ArrowDown':
+                setCurrentIndex(prev=> prev+1);
+                break;
         }
+    }
+    useEffect(() => {
         document.addEventListener('keydown', handleKeyDown)
         return () => {
             document.removeEventListener('keydown', handleKeyDown)
         }
-    }, [])
+    }, [resultData])
     return (
         <>
             <div className="bg-slate-50 px-3 rounded-bl-lg rounded-br-lg -mt-[7px]" >
+
+                {currentIndex}
                 {resultData.map((item) => (
                     <div key={item.id} className="p-2 border-b text-[16px] text-slate-700 border-slate-200 truncate mb-2 ">
                         {item.content}
