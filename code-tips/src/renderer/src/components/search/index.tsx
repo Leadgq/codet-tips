73179b73a830562031b57d10df35cd7100f67fdf
Leadgq <2643336540@qq.com>
2026-01-17 11:22:08 +0800

    feat(搜索组件): 添加设置按钮点击事件并调整样式
    添加设置按钮的点击事件处理函数，调整搜索框和设置按钮的布局样式

--- code-tips/src/renderer/src/components/search/index.tsx
+++ code-tips/src/renderer/src/components/search/index.tsx
@@ -1,7 +1,7 @@
 import { data as codes } from "@renderer/Data"
 import { useCodeStore } from "@renderer/store"
-import {SettingOne} from '@icon-park/react';
- 
+import { SettingOne } from '@icon-park/react';
+
 export function Search(): React.JSX.Element {
 
     const { searchText, setSearchText, setResultData } = useCodeStore((state) => state)
@@ -19,13 +19,14 @@ export function Search(): React.JSX.Element {
             setResultData([])
         }
     }
+    function goSettingPage() {
+        alert('设置')
+    }
     return (
         <>
-            <div className=" bg-slate-50 p-3 drag  rounded-lg ">
-                <div  className="flex items-center w-full">
-                     <SettingOne theme="outline" size="20" fill="#34495e" />
-                </div>
-                <section className=" bg-slate-200 p-2 rounded-lg">
+            <div className=" bg-slate-50 p-3  drag  rounded-lg ">
+                <section className=" bg-slate-200 p-2 rounded-lg flex items-center nodrag">
+                    <SettingOne theme="outline" size="20" className="cursor-pointer" fill="#34495e" onClick={goSettingPage} />
                     <input className="w-full outline-none text-2xl text-slate-600  bg-slate-200"
                         value={searchText}
                         onChange={searchCodeText} />
