1da7271738af889215e6a45833cedc1a2d8b64c8
Leadgq <2643336540@qq.com>
2026-01-22 13:55:05 +0800

    style(页面样式): 调整分类和内容页面的样式及布局
    - 为列表和分类区域添加背景色
    - 调整导航栏高度并添加图标
    - 统一文本大小和颜色
    - 优化网格布局和间距

--- code-tips/src/renderer/src/Pages/Category/index.tsx
+++ code-tips/src/renderer/src/Pages/Category/index.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router'
 import './category.scss'
+import { Add,DatabaseSetting } from '@icon-park/react'
 
 function Category(): React.JSX.Element {
     return (
         <main className="categoryPage">
             <div className="category">分类</div>
-            <div className="nav">导航</div>
+            <div className="nav">
+                <Add theme='outline' size="20" strokeWidth={2} />
+                <DatabaseSetting theme='outline' size="20" strokeWidth={2} />
+            </div>
             <div className="content">
                 <Outlet />
             </div>
