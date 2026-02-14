203a6bf317c3ad784b5bb63f0fa79227be24673c
Leadgq <2643336540@qq.com>
2026-02-14 20:26:47 +0800

    feat(Content): 添加保存按钮并调整页面布局
    - 在内容页面底部添加保存按钮
    - 调整网格布局为三行结构
    - 为输入框和文本域添加背景色和内边距

--- code-tips/src/renderer/src/Pages/Content/index.tsx
+++ code-tips/src/renderer/src/Pages/Content/index.tsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router"
 import './content.scss'
+import { Button } from "antd"
 
 function Content() {
     const content = useLoaderData() as contentType
@@ -7,6 +8,9 @@ function Content() {
         <main className='content-page'>
             <input defaultValue={content.title} />
             <textarea defaultValue={content.content} />
+            <div className="flex justify-center items-center border-t">
+                <Button type="default" size="small">保存</Button>
+            </div>
         </main>
     )
 }
