9376ed26da12163f20945a31790bc7e8aed78099
Leadgq <2643336540@qq.com>
2026-02-14 20:16:25 +0800

    feat(Content): 将内容页布局改为输入框和文本域
    重构内容页布局，移除原有的标题和内容显示方式，改为可编辑的输入框和文本域，提升用户交互体验。

--- code-tips/src/renderer/src/Pages/Content/index.tsx
+++ code-tips/src/renderer/src/Pages/Content/index.tsx
@@ -5,10 +5,8 @@ function Content() {
     const content = useLoaderData() as contentType
     return (
         <main className='content-page'>
-            <h1 className='title'>{content.title}</h1>
-            <div className='page-content'>
-                {content.content}
-            </div>
+            <input defaultValue={content.title} />
+            <textarea defaultValue={content.content} />
         </main>
     )
 }
